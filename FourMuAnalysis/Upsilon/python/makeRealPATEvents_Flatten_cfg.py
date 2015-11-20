a8e9a01ee9a73f1c00d5051f39a36dadb7832637
griley4 <griley4@vols.utk.edu>
2015-11-20 10:46:33 -0500

    Both python configs needed some work. These versions work in my cmssw_5_3_31 setup on newton. note Global Tag change espeially

--- FourMuAnalysis/Upsilon/python/makeRealPATEvents_Flatten_cfg.py
+++ FourMuAnalysis/Upsilon/python/makeRealPATEvents_Flatten_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("PAT")
+process = cms.Process("flatten")
 
 ## MessageLogger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -11,9 +11,67 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) 
 ## Source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-#      'file:/uscms_data/d3/griley4/UpsilonTest/CMSSW_5_3_7_patch5/src/30672037-6946-E211-88CB-001D09F2447F.root',
-     'file:/uscms_data/d2/ayork/CMSSW_5_3_7_patch6/src/Run2012D_sample.root',
-#      '/store/data/Run2011B/MuOnia/AOD/PromptReco-v1/000/178/078/06BDD33D-28F3-E011-A739-003048CF9B28.root',
+#     'file:/uscms_data/d3/griley4/UpsilonTest/HuntForMoreStatistics/CMSSW_5_3_7_patch5/src/RunD-2012-MuOnia/MuOnia_Run2012D_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim.root',
+*(
+
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_100_1_ydA.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_101_1_Yi9.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_102_1_PUX.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_103_1_Ref.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_104_1_UqM.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_105_1_OCO.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_106_1_cNm.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_107_1_Qp3.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_108_1_RGu.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_109_1_snN.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_10_1_G7J.root ',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_110_1_tbJ.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_111_1_1MG.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_112_1_bEs.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_113_1_tW2.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_114_1_ZO7.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_115_1_I5e.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_116_1_RsT.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_117_1_bkv.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_118_1_9lM.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_119_1_b0T.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_11_1_ttm.root ',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_120_1_sp5.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_121_1_VvL.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_122_1_8R7.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_123_1_Vx7.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_124_1_YwM.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_125_2_3Zb.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_126_1_IZA.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_127_1_pzW.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_128_1_8sH.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_129_1_0mz.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_12_1_3nJ.root ',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_130_1_4Gb.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_131_1_1YL.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_132_1_sIq.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_133_1_738.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_134_2_tj1.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_135_2_8ma.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_136_1_GY9.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_137_2_au6.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_138_2_amL.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_139_2_uNn.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_13_1_jhL.root ',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_140_2_ulg.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_141_2_Aqw.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_142_1_jVC.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_143_2_PwE.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_144_2_1DE.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_145_1_9hU.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_146_1_QfW.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_147_1_1JU.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_148_2_uXA.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_149_1_9Cm.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_14_1_1SY.root ',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_150_1_cTQ.root',
+'file:/data/cms_data3/2012_MuOnia_HLTSkim/MuOnia_Run2012A-22Jan2013-v1_HLTSkim/MuOnia_Run2012A_22Jan2013-v1_HLT_JpsiORUpsilon_Oct2015Skim_151_1_Lii.root',
+)
     )
 )
 ## Maximal Number of Events
@@ -23,130 +81,47 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = cms.string('FT_53_V21_AN6::All')
+process.GlobalTag.globaltag = cms.string('FT53_V21A_AN6::All')
 
-## Propagators used for muon matching to HLT
-process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
-process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi")
-process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi")
-process.load("RecoMuon.DetLayers.muonDetLayerGeometry_cfi")
 
 ## Standard PAT Configuration File
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
-
+##Switch on HLT?
+from PhysicsTools.PatAlgos.tools.trigTools import *
+switchOnTriggerStandAlone( process, None, 'patDefaultSequence', 'HLT', '' )
+#switchOnTriggerMatchEmbedding( process, [''], 'patTrigger', 'patDefaultSequence', 'HLT', '' )
 ## Turn off MC matching for real data
 from PhysicsTools.PatAlgos.tools.coreTools import *
 removeMCMatching(process, names=['All'],outputModules=[])
-
-## Switch jet collection
-from PhysicsTools.PatAlgos.tools.jetTools import *
-switchJetCollection(process, 
-                    cms.InputTag('ak5PFJets'),   
-                    doJTA            = True,
-                    doBTagging       = True,
-                    jetCorrLabel     = ('AK5PF', ['L2Relative', 'L3Absolute']),
-                    doType1MET       = False,
-                    genJetCollection = cms.InputTag("ak5GenJets"),
-                    doJetID          = True,
-                    jetIdLabel       = "ak5",
-                    outputModules    = [])
-
-process.patJets.addTagInfos    = True
-process.patJets.tagInfoSources  = cms.VInputTag(
-  cms.InputTag("secondaryVertexTagInfosAOD") )
-process.patJetCorrFactors.useRho    = False
-process.patJetCorrFactors.rho       = cms.InputTag('')
-
-## Add MET from particle flow
-from PhysicsTools.PatAlgos.tools.metTools import *
-addPfMET(process, 'PF')
-
-## Add trigger information
-process.JetMatch = cms.EDProducer("PATTriggerMatcherDRLessByR", # match by DeltaR only, best match by DeltaR
-  src     = cms.InputTag( "cleanPatJets" ),
-  matched = cms.InputTag( "patTrigger" ), # defined in PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
-  andOr                      = cms.bool( False ),  # AND
-  filterIdsEnum              = cms.vstring( '*' ), # wildcard, overlaps with 'filterIds'
-  filterIds                  = cms.vint32( 0 ),    # wildcard, overlaps with 'filterIdsEnum'
-  filterLabels               = cms.vstring( '*' ), # wildcard
-  matchedCuts                = cms.string('type("TriggerJet")'),
-  pathNames                  = cms.vstring('HLT_Jet30U_v*','HLT_Jet60_v*','HLT_Jet80_v*','HLT_Jet110_v*','HLT_Jet150_v*','HLT_Jet190_v*','HLT_Jet240_v*','HLT_Jet370_v*'),
-  pathLastFilterAcceptedOnly = cms.bool( False ),  # select only trigger objects used in last filters of succeeding paths
-  collectionTags             = cms.vstring( '*' ), # wildcard
-  maxDeltaR                  = cms.double( 0.5 ),
-  resolveAmbiguities         = cms.bool( True ),   # only one match per trigger object
-  resolveByMatchQuality      = cms.bool( True )    # take best match found per reco object: by DeltaR here (s. above)
-)
-
-from PhysicsTools.PatAlgos.tools.trigTools import *
-switchOnTriggerStandAlone( process, None, 'patDefaultSequence', 'HLT', '' )
-switchOnTriggerMatchEmbedding( process, ['JetMatch'], 'patTrigger', 'patDefaultSequence', 'HLT', '' )
-
-## Trigger match the Muons
-process.load("MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff")
-import PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi
-process.patMuonsWithoutTrigger = PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi.patMuons.clone()
-
-from MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff import addMCinfo, changeRecoMuonInput, useL1MatchingWindowForSinglets, changeTriggerProcessName, switchOffAmbiguityResolution
-# with some customization
-#addMCinfo(process)
-# since we match inner tracks, keep the matching tight and make it one-to-one
-#process.muonMatch.maxDeltaR = 0.05
-#process.muonMatch.resolveByMatchQuality = True
-#process.muonMatch.matched = "genMuons"
-#changeRecoMuonInput(process, "mergedMuons")
-useL1MatchingWindowForSinglets(process)
-#changeTriggerProcessName(process, "REDIGI36X")
-switchOffAmbiguityResolution(process) # Switch off ambiguity resolution: allow multiple reco muons to match to the same trigger muon
-process.muonMatchHLTL3.maxDeltaR = 0.1
-process.muonMatchHLTL3.maxDPtRel = 10.0
-process.muonMatchHLTCtfTrack.maxDeltaR = 0.1
-process.muonMatchHLTCtfTrack.maxDPtRel = 10.0
-process.muonMatchHLTTrackMu.maxDeltaR = 0.1
-process.muonMatchHLTTrackMu.maxDPtRel = 10.0
-
 ## Make JPsi and Higgs candidates
 process.goodMuons = cms.EDFilter("MuonRefSelector",
     src = cms.InputTag("muons"),
     cut = cms.string(""),
 )
-process.JPsiToMuMu = cms.EDProducer("CandViewShallowCloneCombiner",
+process.Dimuon = cms.EDProducer("CandViewShallowCloneCombiner",
     decay = cms.string('goodMuons@+ goodMuons@-'),
     cut = cms.string(''),
 )
-
+process.Fourmuon = cms.EDProducer("CandViewShallowCloneCombiner",
+    decay= cms.string('Dimuon Dimuon'),
+    cut = cms.string(''),
+)
 
 ## Analyzer package
 process.PATEventTree = cms.EDAnalyzer("PATEventTree",
-    rootFileName                 = cms.untracked.string("MuOnia_Run2012A_22Jan2013-v1_JPsi-Upsilon_advMuonReq.root"),
-    HLT_Skim                     = cms.untracked.bool(True),
-    isEtabJPsi                   = cms.untracked.bool(False),
-    isJPsiMuMu                   = cms.untracked.bool(False),
-    isHBB                        = cms.untracked.bool(False),
+    rootFileName                 = cms.untracked.string("Run2012Test_Flattened.root"),
     isMC                         = cms.untracked.bool(False),
-    useFatJets                   = cms.untracked.bool(False),
-    PrimaryVertexCollectionLabel = cms.untracked.InputTag('offlinePrimaryVertices'),
     HLTCollectionLabel           = cms.untracked.InputTag('patTrigger'),
-    MuonCollectionLabel          = cms.untracked.InputTag('patMuonsWithTrigger'),
-    ElectronCollectionLabel      = cms.untracked.InputTag('cleanPatElectrons'),
-    PhotonCollectionLabel        = cms.untracked.InputTag('cleanPatPhotons'),
-#    ParticleCollectionLabel      = cms.untracked.InputTag('cleanPatTrackCands'),
-    TrackCollectionLabel         = cms.untracked.InputTag('generalTracks'),
-    JetCollectionLabel           = cms.untracked.InputTag('cleanPatJetsTriggerMatch'),
-    FatJetCollectionLabel        = cms.untracked.VInputTag(['cleanPatJetsPFFAT','cleanPatJetsPFSUB','cleanPatJetsPFFILTER']),
-    METCollectionLabel           = cms.untracked.InputTag('patMETsPF'),
+    MuonCollectionLabel          = cms.untracked.InputTag('patMuons'),
     JPsiInputLabel               = cms.untracked.InputTag('goodMuons'),
-    JPsiCandLabel                = cms.untracked.InputTag('JPsiToMuMu'),
+    DimuonCandLabel                = cms.untracked.InputTag('Dimuon'),
+    FourmuonCandLabel                = cms.untracked.InputTag('Fourmuon'),
     GenCollectionLabel           = cms.untracked.InputTag('genParticles')
     )
-
-
 ## Run the actual sequence
-process.patDefaultSequence.replace(process.patMuons,process.patMuonsWithoutTrigger * process.patTriggerMatching * process.patMuons)
 process.p = cms.Path(
-    process.patDefaultSequence*
-    process.patMuonsWithTriggerSequence*
     process.goodMuons*
-    process.JPsiToMuMu*
+    process.Dimuon*
+    process.Fourmuon*
     process.PATEventTree
     )
